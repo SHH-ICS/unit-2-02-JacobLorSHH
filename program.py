34f6f8638c2ff1c9fe73b1b2d4ce62b1997061ec
JacobLorSHH <144691606+JacobLorSHH@users.noreply.github.com>
2023-10-03 13:20:11 +0000

    Extra

--- program.py
+++ program.py
@@ -1,6 +1,11 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
-  return "Hello World!"
+def myFunction(name=""):
+  return f"Hello, {name}!"
   # My Function
+
+
+username = "jacob"
+print(myFunction(username))
+
