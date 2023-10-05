d0726d6b608affb4d8589070c0eca69c8a2bc211
JacobLorSHH <144691606+JacobLorSHH@users.noreply.github.com>
2023-10-05 13:29:47 +0000

    Extra 2-03

--- program.py
+++ program.py
@@ -1,11 +1,13 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction(name=""):
-  return f"Hello, {name}!"
-  # My Function
+def myFunction(name="", age=0, HadBirthday=None):
+  if HadBirthday == True:
+    age += 1  
+  return name + "was born in" + (age - 2023)
+#MyFunction
 
 
-username = "jacob"
-print(myFunction(username))
-
+Birth = input('have you had your birthday this year?')
+if Birth.lower == 'y':
+  Birthday
